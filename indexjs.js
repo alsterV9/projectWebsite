591351679bb6f1a368d06d5af2e7a88fa4cd8313
alsterV9 <albertussello244@gmail.com>
2025-11-23 11:02:33 +0200

    js functionality intro

--- indexjs.js
+++ indexjs.js
@@ -1,4 +1,5 @@
 const rdot = document.getElementById("rdot");
+const secsel = document.getElementById("secsel");
 
 const lungo = document.getElementById("lungo"),flatwhite=document.getElementById("flatwhite");
 const affogato = document.getElementById("affogato"),amaricano=document.getElementById("amaricano");
@@ -6,20 +7,21 @@ const latte = document.getElementById("latte"),cupicino=document.getElementById(
 
 let arr = [];
 
-lungo.addEventListener('click', orderer);
-
 let coffee = [
-    {"id": "Lungo", "price":13.90},
-    {"id":"Flat White", "price":12.05},
-    {"id":"Affogato", "price":13.20},
-    {"id":"Amaricano", "price":14.10},
-    {"id":"Latte", "price":15.50},
-    {"id":"Cupicino", "price":13.90}
+    {id:"Lungo", price:13.90, stock:true},
+    {id:"Flat White", price:12.05, stock:true},
+    {id:"Affogato", price:13.20, stock:true},
+    {id:"Amaricano", price:14.10, stock:true},
+    {id:"Latte", price:15.50 , stock:true},
+    {id:"Cupicino", price:13.90, stock:true},
+    {id:"not instock", price:"out of stock"}
 ]
 
-function orderer(e){
+secsel.addEventListener('click', (e)=>{
     arr.push('item');
     rdot.innerText = arr.length;
     console.log(arr);
-    console.log(e.target.id)
-}
+    console.log(e)
+});
+
+console.log(coffee[2].id);
