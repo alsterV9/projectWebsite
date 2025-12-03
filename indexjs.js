5d8a4cd380e21c88d93845bf380bc34c074580a4
alsterV9 <albertussello244@gmail.com>
2025-12-03 13:02:30 +0200

    works on cart functionality

--- indexjs.js
+++ indexjs.js
@@ -1,31 +1,63 @@
+
+
 const rdot = document.getElementById("rdot");
 const secsel = document.getElementById("secsel");
-
+const incart = document.getElementById("incart");
 const lungo = document.getElementById("lungo"),flatwhite=document.getElementById("flatwhite");
 const affogato = document.getElementById("affogato"),amaricano=document.getElementById("amaricano");
 const latte = document.getElementById("latte"),cupicino=document.getElementById("cupicino");
 
+
+function buildcart(){
+    /*const cartcont = document.createElement('div'); */
+    const btn = document.createElement('button');
+    btn.classList.add('cartbtn');
+    btn.innerText = "clear cart items";
+    incart.appendChild(btn);
+
+}
+
+
 let arr = [];
+ 
+
+let arr1 = [];
+let totalprice = 0;
 
 let coffee = [
-    {id:"Lungo", price:13.90, stock:true},
-    {id:"Flat White", price:12.05, stock:true},
-    {id:"Affogato", price:13.20, stock:true},
-    {id:"Amaricano", price:14.10, stock:true},
-    {id:"Latte", price:15.50 , stock:true},
-    {id:"Cupicino", price:13.90, stock:true},
-    {id:"not instock", price:"out of stock"}
+    {id:"Lungo", price:13, stock:true},
+    {id:"Flat White", price:12, stock:true},
+    {id:"Affogato", price:13, stock:true},
+    {id:"Amaricano", price:14, stock:true},
+    {id:"Latte", price:15, stock:true},
+    {id:"Cupocino", price:13, stock:true},
+    {id:"not instock", stock: false}
 ]
 
-/*secsel.addEventListener('click', (e)=>{
-    arr.push('item');
-    rdot.innerText = arr.length;
-    console.log(arr);
-    console.log(e)
-}); */
 function coffeefunc(id){
-    arr.push(coffee[id].id);
+    if(arr.length<10){
+        arr.push(coffee[id].id);
+    if(arr1.length <1){
+        arr1.push(coffee[id].price);
+    }else{
+        arr1[0] += coffee[id].price;
+    };
     rdot.innerText = arr.length;
-    console.log(arr);
-    console.log(coffee[id].id + " costs: " + coffee[id].price);
+    console.log(coffee[id].id +" "+coffee[id].price);
+    
+    thap();
+    buildcart();
+       
+    
+    } else{alert("Your Cart is full")}
 } 
+/*
+create array -> takes id plus price -> function that check if there's item >1 if true 
+take total of same= j, j x price of item
+*/
+function thap(){
+    totalprice = arr1[0];
+    incart.innerText=totalprice;
+    
+    
+}
