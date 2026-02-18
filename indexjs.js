cb6b8cc0799cf2b3e615ed05fb30d9ec7dba494b
alsterV9 <albertussello244@gmail.com>
2026-02-18 18:13:38 +0200

    main functionality done on cart menu

--- indexjs.js
+++ indexjs.js
@@ -1,6 +1,6 @@
 const rdot = document.getElementById("rdot");
 const secsel = document.getElementById("secsel"),cont=document.getElementById("cont");
-const incart = document.getElementById("innercont");
+const incart = document.getElementById("innercont"),totalPrice = document.getElementById("totalPrice");
 const lungo = document.getElementById("lungo"),flatwhite=document.getElementById("flatwhite");
 const affogato = document.getElementById("affogato"),amaricano=document.getElementById("amaricano");
 const latte = document.getElementById("latte"),cappuccino=document.getElementById("cappuccino");
@@ -11,12 +11,12 @@ let arr1 = [];
 let totalprice = 0;
 
 let coffee = [
-    {id:"Lungo", price:13, stock:true},
-    {id:"Flat White", price:12, stock:true},
-    {id:"Affogato", price:13, stock:true},
-    {id:"Amaricano", price:14, stock:true},
-    {id:"Latte", price:15, stock:true},
-    {id:"Cappuccino", price:13, stock:true},
+    {id:"Lungo", price:37, stock:true},
+    {id:"Flat White", price:23, stock:true},
+    {id:"Affogato", price:26, stock:true},
+    {id:"Amaricano", price:30, stock:true},
+    {id:"Latte", price:33, stock:true},
+    {id:"Cappuccino", price:34, stock:true},
     {id:"not instock", stock: false, pattofile:"https://rlv.zcache.com/error_404_coffee_not_found_funny_mug-r903e5cb138fe47569dbdf40a39b4c818_kz92h_166.jpg?rlvnet=1"}
 ]
 function togglecart(){
@@ -27,21 +27,44 @@ function togglecart(){
     }
 }
 
-onload = hidecart();
+//onload = hidecart();
 function coffeefunc(id){
     if(arr.length<10){
         arr.push(coffee[id].id +" R"+coffee[id].price);
         console.log(arr);
         
-        
+            const div = document.createElement("div");
+             div.classList.add("cartitem");
+             const img = document.createElement("img");
+             img.src = coffee[id].pattofile || "coffee pic.png";
+             img.style.width = "50px";
+             div.append(img);
+             const textblock = document.createElement("span");
+             textblock.classList.add("textblock");
+             textblock.textContent = coffee[id].id +" R"+coffee[id].price;
+             div.append(textblock);
             const btn = document.createElement("button");
-            btn.textContent = arr[arr.length -1];
-            incart.append(btn);
+            btn.classList.add("removebtn");
+            btn.onclick = function() {
+                div.remove();
+                arr.splice(arr.indexOf(coffee[id].id +" R"+coffee[id].price), 1);
+                arr1[0] -= coffee[id].price;
+                totalPrice.innerText = "Total: R "+arr1[0];
+                rdot.innerText = arr.length;
+            };
+            btn.textContent = "remove this item"
+            div.append(btn);
+            incart.append(div);
         
     if(arr1.length <1){
         arr1.push(coffee[id].price);
+        totalPrice.innerText = arr1[0];
+        console.log(totalPrice.innerText);
     }else{
         arr1[0] += coffee[id].price;
+        totalPrice.innerText = "Total: R"+arr1[0];
+        console.log(arr1);
+        console.log(totalPrice.innerText);
     };
     rdot.innerText = arr.length;
     
@@ -49,13 +72,19 @@ function coffeefunc(id){
 } 
 
 function removebtn(){
-    if(arr.length > 0){
-        arr.pop();
-        arr1.pop();
-        rdot.innerText = arr.length;
-        if(incart.hasChildNodes){
-        incart.removeChild(incart.lastChild);
-    }
-    }
+    
     
 }
+document.querySelectorAll('.removebtn').forEach(function(el) {
+    el.addEventListener('click', function() {
+      try {
+        // Safely remove the clicked element
+        //this.remove(); // Modern browsers
+        // Alternative for older browsers:
+        console.log("Attempting to remove element:", this);
+         this.parentNode.removeChild(this);
+      } catch (err) {
+        console.error("Error removing element:", err);
+      }
+    });
+  });
